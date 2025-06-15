f14dfd63b61a911e35361f6ed2a52945fe710b23
Veritas-lab <veritas91@list.ru>
2025-06-15 17:45:51 +0300

    Внесены правки

---
+++ modules/comments.js
@@ -0,0 +1,15 @@
+export let comments = [];
+
+export const updateComments = newComments => {
+  comments = newComments
+}
+
+export const addComment = (newComment) => {
+  comments.push(newComment);
+};
+
+export const toggleLike = (index) => {
+  const comment = comments[index];
+  comment.likes = comment.isLiked ? comment.likes - 1 : comment.likes + 1;
+  comment.isLiked = !comment.isLiked;
+};
